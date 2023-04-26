178c4d670b90e915dfb1eba173fe24886289da23
MrAdhit <adhityarimba97@gmail.com>
2023-04-26 08:49:20 +0700

    Revert formatting

--- src/main/java/com/mradhit/lorevoter/command/LoreVoteCommand.java
+++ src/main/java/com/mradhit/lorevoter/command/LoreVoteCommand.java
@@ -17,8 +17,7 @@ public class LoreVoteCommand implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         try {
-            if (args[0] == null)
-                return true;
+            if (args[0] == null) return true;
             String sub = args[0];
 
             if (sub.equalsIgnoreCase("reload")) {
@@ -41,13 +40,11 @@ public class LoreVoteCommand implements CommandExecutor {
             }
 
             if (sub.equalsIgnoreCase("party")) {
-                if (args[1] == null)
-                    return true;
+                if (args[1] == null) return true;
                 String sub1 = args[1];
 
                 if (sub1.equalsIgnoreCase("set")) {
-                    if (args[2] == null)
-                        return true;
+                    if (args[2] == null) return true;
                     String sub2 = args[2];
 
                     int amount = Integer.parseInt(sub2);
@@ -68,19 +65,16 @@ public class LoreVoteCommand implements CommandExecutor {
             }
 
             if (sub.equalsIgnoreCase("player")) {
-                if (args[1] == null)
-                    return true;
+                if (args[1] == null) return true;
                 String sub1 = args[1];
 
-                if (args[2] == null)
-                    return true;
+                if (args[2] == null) return true;
                 String sub2 = args[2];
 
                 PlayerVoteManager playerVoteManager = new PlayerVoteManager(sub1);
 
                 if (sub2.equalsIgnoreCase("set")) {
-                    if (args[3] == null)
-                        return true;
+                    if (args[3] == null) return true;
                     String sub3 = args[3];
 
                     int amount = Integer.parseInt(sub3);
@@ -103,9 +97,7 @@ public class LoreVoteCommand implements CommandExecutor {
                 }
             }
             return false;
-        } catch (
-
-        IndexOutOfBoundsException e) {
+        } catch (IndexOutOfBoundsException e) {
             return true;
         }
     }
